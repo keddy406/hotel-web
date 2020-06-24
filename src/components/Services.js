5932d8bc983662a10152f696d49c1c775a13cde1
keddy406 <54757799+keddy406@users.noreply.github.com>
2020-06-24 23:17:52 +0800

    feature room with context api and proptypes

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Title from "./Titles";
+import Title from "./Title";
 import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
 
 export default class extends Component {
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Titles({ title }) {
+export default function Title({ title }) {
   return (
     <div className="section-title">
       <h4>{title}</h4>
