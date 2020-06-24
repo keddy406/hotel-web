e604536705268c36304247966b8e1cf0c7235dea
keddy406 <54757799+keddy406@users.noreply.github.com>
2020-06-24 19:35:06 +0800

    setup context

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import Services from "../components/Services";
+import FeatureRooms from '../components/FeaturedRooms'
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
         </Banner>
       </Hero>
       <Services />
+      <FeatureRooms />
     </>
   );
 }
