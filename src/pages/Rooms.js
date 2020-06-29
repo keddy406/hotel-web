ae3bdbfc0cf2afa51e31f03d6c964f6dac4ba7e0
keddy406 <54757799+keddy406@users.noreply.github.com>
2020-06-29 22:35:16 +0800

    setting all app with local data

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,8 +2,11 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import {Link} from 'react-router-dom';
+import RoomsContainer from '../components/RoomContainer'
+
 export default function Rooms() {
     return(
+        <>
         <Hero hero="roomsHero">
             <Banner title="our rooms">
                 <Link to="/" className="btn-primary">
@@ -12,5 +15,7 @@ export default function Rooms() {
 
             </Banner>
             </Hero>
+        <RoomsContainer />
+        </>
         )
 }
